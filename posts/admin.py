a34e6331c35269e8ae44d5996f41487cc45de9b6
vibhor <vats.vibhor.07@gmail.com>
2020-03-15 13:57:51 +0530

    Registerd Post to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "last_updated"]
+    list_filter = ['timestamp', 'last_updated']
+
+    search_fields = ['title', 'content']
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
