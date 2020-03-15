41fe516af81518df547c0c7a40ded03bd6edef71
vibhor <vats.vibhor.07@gmail.com>
2020-03-15 17:28:59 +0530

    basic curd opertions(urlpatterns)

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+   path('', views.post_view),
+   path('create/', views.post_create),
+   path('details/', views.post_details),
+   path('update/', views.post_update),
+   path('delete/', views.post_delete),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def post_view(request):
+    return HttpResponse("<h2>Home</h2>")
+
+
+def post_create(request):
+    return HttpResponse("<h2>Create</h2>")
+
+
+def post_details(request):
+    return HttpResponse("<h2>Details</h2>")
+
+
+def post_update(request):
+    return HttpResponse("<h2>Update</h2>")
+
+    
+def post_delete(request):
+    return HttpResponse("<h2>Delete</h2>")
